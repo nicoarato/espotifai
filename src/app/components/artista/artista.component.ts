1e95a020c73be6c0b34391860f850f50f9ff6d33
Nico Arato <nico.arato@gmail.com>
2020-02-15 20:46:30 -0300

    Se crea componente de artista.

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-artista',
   templateUrl: './artista.component.html',
   styles: []
 })
-export class ArtistaComponent implements OnInit {
+export class ArtistaComponent {
 
-  constructor() { }
+  artista: any = {};
+  loadingArtist: boolean;
+  constructor(private router: ActivatedRoute,
+              private spotify: SpotifyService) {
+    this.loadingArtist = true;
+    this.router.params.subscribe( params => {
+      this.getArtista( params['id']);
 
-  ngOnInit() {
+    });
+  }
+
+  getArtista( id: string) {
+
+    this.loadingArtist = true;
+
+    this.spotify.getArtista(id).subscribe(artista => {
+          console.log(artista);
+          this.artista = artista;
+          this.loadingArtist = false;
+        });
   }
 
 }
